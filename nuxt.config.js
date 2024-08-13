48eb55f346626035d652333925320e0d73acba37
Zeyad Shohbor <zeyadmoamen7@gmail.com>
2024-08-13 14:21:56 +0300

    Change base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://namaa-site.onrender.com',
+    baseURL: 'http://api.namaatec.com',
     // headers: {
     //   'Access-Control-Allow-Origin': '*'
     // }
